60bcdf6ac46a95396f9c3e42e7d1db6ec55bd4d8
LuanDL <duanlias@gmail.com>
2025-02-25 18:32:19 -0300

    teste

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.ImprimirTabuleiro(tab);
+
+            Console.ReadLine();
+        }
+    }
+}
